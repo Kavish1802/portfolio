6be8d957da600885bc3280e50f3edf4bd41a1d87
kavishgupta1802 <btech10295.19@bitmesra.ac.in>
2024-02-15 01:10:09 +0530

    started making about page, added starcanvas in its background, yet the not satisfied with the bg, div from about.jsx has created a blury/translucent kind of a background, will try to figure out some solution around it. So its a wrap for today , time to sleep

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,41 @@
 /* eslint-disable no-unused-vars */
+import { Stars } from '@react-three/drei'
 import React from 'react'
+import StarsCanvas from '../models/StarsCanvas'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <StarsCanvas/>
+      <h1 className='head-text'>
+        Hello, I am <span className='blue-gradient_text font-semibold drop-shadow'>
+          Kavish
+        </span>
+      </h1>
+      <div className='flex flex-col gap-3 mt-5 text-slate-500'>
+        <p>
+          I am a software engineer, currently working at WiseTech Global as a full stack developer,
+          passionate about software engineering and always looking for new opportunities to learn and grow.
+        </p>
+      </div>
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>
+         My Skills
+        </h3>
+
+        <div className='flex flex-col gap-3 mt-5 text-slate-500'>
+          <p>
+            I have experience in working with the following technologies:
+          </p>
+          <ul className='list-disc list-inside'>
+            <li>React</li>
+            <li>C++</li>
+            <li>Java</li>
+            <li>SpringBoot</li>
+          </ul>
+        </div>
+      </div>
+    </section>
   )
 }
 
