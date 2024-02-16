a773827b4df4c05f0a1c2a837a418ebd59d383c0
kavishgupta1802 <btech10295.19@bitmesra.ac.in>
2024-02-16 19:56:17 +0530

    try:3

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude:['**/*.glb'],
-  build: {
-    rollupOptions: {
-      external: ['react-vertical-timeline-component'],
-    },
-  },
+  rollupOptions:{
+    external:['react-vertical-timeline-component']
+  }
 })
