94a9786c394ad1632c91503882bae90b52fe9796
kavishgupta1802 <btech10295.19@bitmesra.ac.in>
2024-02-16 20:03:44 +0530

    try-5

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import StarsCanvas from '../models/StarsCanvas'
 import { skills, experiences } from '../constants'
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
-import 'react-vertical-timeline-component/style.min.css';
+import '../index.css';
 
 const About = () => {
   return (
