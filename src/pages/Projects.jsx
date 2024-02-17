69b9748e050ca9d7069c686142b56a76bd011c2d
kavishgupta1802 <btech10295.19@bitmesra.ac.in>
2024-02-17 17:50:33 +0530

    completed projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,8 @@ import { projects, experiences } from '../constants'
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
 import '../index.css';
 import CTA from '../components/CTA'
+import { Link } from 'react-router-dom';
+import { arrow } from '../assets/icons';
 
 const Projects = () => {
   return (
@@ -27,10 +29,10 @@ const Projects = () => {
     <div className='flex flex-wrap my-20 gap-16'>
       {projects.map((project) => (
         // eslint-disable-next-line react/jsx-key
-        <div>
-          <div>
+        <div className='lg:w-[400px] w-full' key={project.name}>
+          <div className='block-container w-12 h-12'>
             <div className={`btn-back rounded-xl ${project.theme}`}/>
-            <div className='lg:w-[400px] w-full' key={project.name}>
+            <div className='btn-front rounded-xl flex justify-center items-center' >
               <img
                 src={project.iconUrl}
                 alt="project name"
@@ -38,6 +40,29 @@ const Projects = () => {
               />
             </div>
           </div>
+
+          <div className='flex flex-col'>
+            <h4 className='text-2xl font-poppins font-semibold'>{project.name}</h4>
+            <p className='mt-2 text-slate-400'>
+              {project.description}
+            </p>
+            <div className='mt-5 flex items-center gap-2 font-poppins'>
+              <Link
+               to={project.link}
+               target="_blank"
+               rel="noopener noreferrer"
+               className='font-semibold text-blue-600'
+              >
+              Live Link
+              </Link>
+               <img
+                src={arrow}
+                alt="arrow"
+                className='w-4 h-4 object-contain'
+               />
+            </div>
+          </div>
+
         </div>
        ))}
     </div>
