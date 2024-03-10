ee8cad80b4ca868939cf979072fee8ff746478de
kavishgupta1802 <btech10295.19@bitmesra.ac.in>
2024-03-10 22:11:11 +0530

    now the contact page works! message gets sent on my email. Although some UI fix is required. Still, Hurray!

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react'
 import { useRef } from 'react'
+import axios from 'axios'
+import Button from '../components/Button'
 import emailjs from '@emailjs/browser'
 import StarsCanvas from '../models/StarsCanvas';
 
@@ -8,8 +10,8 @@ const Contact = () => {
   const formRef=useRef(null);
   const [form, setForm] = useState({name: '', email: '', message: ''});
   const [isLoading, setIsLoading] = useState(false);
-
-  
+ 
+     
   const handleChange = (e) => { 
     setForm({...form, [e.target.name]: e.target.value});
   }
@@ -20,27 +22,46 @@ const Contact = () => {
   const handleBlur = (e) => {
     e.target.classList.remove('focused');
   }
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    emailjs.send(
-     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-     {
-       from_name:form.name,
-       to_name:"Kavish",
-       from_email:form.email,
-       to_email:'kavishgupta2011@gmail.com',
-       message:form.message  
-     },
-     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    ).then(()=>{
-      setIsLoading(false);
-      setForm({name: '', email: '', message: ''});
-    }).catch((error)=>{
-      setIsLoading(false);
-      console.log(error);
-    })
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setIsLoading(true);
+  //   emailjs.send(
+  //    import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+  //    import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+  //    {
+  //      from_name:form.name,
+  //      to_name:"Kavish",
+  //      from_email:form.email,
+  //      to_email:'kavishgupta2011@gmail.com',
+  //      message:form.message  
+  //    },
+  //    import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+  //   ).then(()=>{
+  //     setIsLoading(false);
+  //     setForm({name: '', email: '', message: ''});
+  //   }).catch((error)=>{
+  //     setIsLoading(false);
+  //     console.log(error);
+  //   })
+  // }
+
+  const submitForm=async () =>{
+    if(form.email.length>0 && form.name.length>0 && form.message.length>0){
+      setIsLoading(true);
+      axios
+        .post("https://formspree.io/f/mqkrldkn", form)
+        .then((response)=>{
+           alert('Message sent successfully'); 
+           console.log(response);
+           setIsLoading(false);
+        })
+        .catch((error)=>{
+          alert('Message not sent, Error : '+error);
+        })
+    }
+    else{
+      alert('Please fill all the fields');
+    }
   }
 
   return (
@@ -53,7 +74,12 @@ const Contact = () => {
            <a className='blue-gradient_text' href="mailto:kavishgupta2011@gmail.com"> kavishgupta2011@gmail.com</a>
         </h3>
          <form
-          onSubmit={handleSubmit} 
+          onSubmit={(e)=>{
+            e.preventDefault();
+            submitForm(e);
+          }
+
+          } 
           className='w-full flex flex-col gap-7 mt-14 '>
            <label className='text-blue-500 font-semibold'>
               Name
@@ -97,14 +123,15 @@ const Contact = () => {
                 onBlur={handleBlur}
                 />
            </label>
-           <button
+           <Button
             type="submit"
             onFocus={handleFocus}
             onBlur={handleBlur}
-            className='btn width-20' 
+            className='btn width-full' 
+            text={isLoading? 'Sending...':'Send Message'}
             > 
-            {isLoading? 'Sending...':'Send Message'}
-           </button> 
+            
+           </Button> 
            
          </form>
       </div>
